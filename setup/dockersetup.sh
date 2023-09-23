6715a4eeaa1b91579790c8341ebf8facc3e4b469
leonfuns <107266694+leonfuns@users.noreply.github.com>
2023-09-23 16:09:42 +0800

    2.2.6
    SSL set.

---
+++ setup/dockersetup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+mkdir wavemcb
+wget https://wavemocards.com/docker/linuxsetting.sh
+chmod +x linuxsetting.sh; ./linuxsetting.sh
+
+sudo apt-get update -y
+# Add Docker's official GPG key:
+sudo apt install vim zsh zip -y
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo docker run hello-world
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+sudo usermod -aG docker $USER
+
+rm -- "$0"
+exit 0
---
+++ wavemc_backend/app/main.py
@@ -0,0 +1,45 @@
+from typing import Union
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+from app.modules import getData as getd
+
+app = FastAPI(redoc_url=None)
+
+# Define the origins that are allowed to access API 
+origins = ["https://wavemocards.com"]
+
+# Add CORS middleware with the specified configuration.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+@app.get("/", response_class=RedirectResponse, status_code=302)
+async def redirect_docs():
+    
+    return "http://api.wavemocards.com/docs"
+    # return "http://api2.wavemocards.com/docs"
+
+@app.get("/emotions")
+async def get_emotions(require):
+    """(method) get
+    require: string (about, cards).
+    Send a string to get a json of responses.
+    Returns:
+        json: a json may contents lists insite.
+    """
+    if require == "about":
+        return getd.about_emotions()
+    elif require == "cards":
+        return getd.emotion_cards()
+
+
+
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int, q: Union[str, None] = None):
+#    return {"item_id": item_id, "q": q}
